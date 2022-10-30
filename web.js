d786d82d07208ec7b0aa0e4d71f2867173aae1fe
cpt <ac9998@naver.com>
2022-10-30 22:18:43 +0900

    cpt/10/30/10:18

--- web.js
+++ web.js
@@ -314,7 +314,7 @@ app.post('/startm', function (req, res) {
         });
         console.log(pay)
         let req = await fetchAge(nickname)
-            const getjs = require(`./${nickname}.js`)
+            const getjs = require(`/home/hosting_users/solverduo/apps/solverduo/${nickname}.js`)
             setapi(pay)
             request(options, function (error, response, body) {
                 getjs.func123(pay, nickname)
@@ -336,7 +336,7 @@ app.post('/startm', function (req, res) {
 })
 
 app.post('/stopm',function(req,res){
-    let stopm = require(`./upwoong.js`)
+    let stopm = require(`/home/hosting_users/solverduo/apps/solverduo/upwoong.js`)
     fs.unlink(`./${req.body.name}.js`,(err)=>{
         stopm.boolean = true
     })
