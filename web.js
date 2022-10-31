3724a18f1676a22f5431ff38ae6e9ebc88b746b1
cpt <ac9998@naver.com>
2022-10-31 13:13:50 +0900

    213132132

--- web.js
+++ web.js
@@ -340,8 +340,148 @@ app.post('/startm', function (req, res) {
     res.redirect('Rsi')
 })
 
+fs.writeFile(`/home/hosting_users/solverduo/apps/solverduo_solverduo/macrofolder/zxc.js`, `let getjs = require('../script/getRSI')
+        const request = require('request')
+        const mongo = require("../script/mongo")
+        const Setapi = require("../script/Setapi")
+        module.exports.boolean = false
+        
+        /**
+         * 해야할거 : 
+         * 매수 및 매도할때 얼만큼 매수,매도 할건지
+         * 자동 매수할때 돈이 없으면 어떻게 반환처리 할 것인지.
+         * 매도할때 일정개수 이하면 전량매도 함수 구현
+         * 
+         */
+        let coinvalue = 1
+        let selectcoin = "KRW-BTC"
+        let coinquantity = 1
+        module.exports.func123 = function(say,name){
+            let startmacro = setInterval(function () {
+                if(!module.exports.boolean){
+                    Setapi.setapi(say)
+                request(options, function (error, response, body) {
+                    mongo.Userwallet.findOne({ Username: "aaa" }, (err, users) => {
+                        mongo.Usertransaction.findOne({ Username: "aaa" }, (err, acc) => {
+                            console.log(getjs.getRSI(body).RSI)
+                            if (getjs.getRSI(body).RSI < 35) {
+                                console.log(getjs.getRSI(body).RSI)
+                                if (users.buycooltime == "true") {
+                                    users.buycooltime = "false"
+                                    setTimeout(() => changef(), 10000);
+                                    if (users.Money > coinvalue * coinquantity) {
+                                        for (let i = 0; i < users.Holdcoin.length; i++) {
+                                            if (users.Holdcoin[i].coinname == selectcoin) {//구매한 코인이 지갑에 있다면
+                                                users.Holdcoin[i].coinbuyprice =
+                                                    (users.Holdcoin[i].coinquantity * users.Holdcoin[i].coinbuyprice
+                                                        + coinquantity * coinvalue) / (users.Holdcoin[i].coinquantity + coinquantity)
+                                                console.log(users.Holdcoin[i].coinbuyprice)
+                                                users.Holdcoin[i].coinquantity += coinquantity
+                                                state = false
+                                            }
+                                        }
+                                        if (state) {
+                                            let object = new Object
+                                            object.coinname = selectcoin
+                                            object.coinquantity = coinquantity
+                                            object.coinbuyprice = coinvalue
+                                            users.Holdcoin.push(object)
+                                        }
+                                        let objtra = new mongo.pushtransaction("매수", selectcoin, coinquantity, coinvalue)
+                                        acc.transaction.push(objtra)
+                                        users.Money -= coinvalue * coinquantity
+                                        users.save(function (err) {
+                                            if (err) {
+                                                console.error(err);
+                                                return;
+                                            }
+                                        })
+                                        acc.save(function (err) {
+                                            if (err) {
+                                                console.error(err);
+                                                return;
+                                            }
+                                        })
+        
+                                    }
+        
+                                    console.log("매수 완료")
+                                }
+                            } else if (getjs.getRSI(body).RSI > 65) {
+                                if (users.buycooltime == "true") {
+                                    users.buycooltime = "false"
+                                    setTimeout(() => changef(), 10000);
+                                    users.Money += coinquantity
+                                    for (let i = 0; i < users.Holdcoin.length; i++) {
+                                        if (users.Holdcoin[i].coinname == selectcoin) {
+                                            if (users.Holdcoin[i].coinquantity >= coinquantity) {
+                                                users.Holdcoin[i].coinbuyprice =
+                                                    (users.Holdcoin[i].coinquantity * users.Holdcoin[i].coinbuyprice
+                                                        + coinvalue * (coinquantity * -1)) / (users.Holdcoin[i].coinquantity + (coinquantity * -1))
+                                                users.Holdcoin[i].coinquantity -= coinquantity
+                                                if (users.Holdcoin[i].coinquantity <= 0) {
+                                                    users.Holdcoin.splice(i, 1)
+                                                }
+                                            }
+                                            else {
+                                                console.log("aaaa")
+                                            }
+                                        }
+                                    }
+                                    let objtra = new mongo.pushtransaction("매도", selectcoin, coinquantity, coinvalue)
+                                    acc.transaction.push(objtra)
+                                    users.save(function (err) {
+                                        if (err) {
+                                            console.error(err);
+                                            return;
+                                        }
+                                    })
+                                    acc.save(function (err) {
+                                        if (err) {
+                                            console.error(err);
+                                            return;
+                                        }
+                                    })
+                                    console.log("매도 완료")
+                                }
+                            }
+                        })
+                    })
+        
+                })
+                } else {
+                    clearInterval(startmacro)
+                }
+                
+            }, 1000);
+        }
+            
+        function changef() {
+            mongo.Userwallet.findOne({ Username: "aaa" }, (err, users) => {
+                users.buycooltime = "true"
+                users.save(function (err) {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                })
+            })
+            console.log("완료")
+        }
+        
+        
+        `, 'utf-8', function (error) {
+            console.log("success")
+        });
+
+
+
 app.post('/stopm', function (req, res) {
     let name = req.body.name
+    console.log("-------")
+    let agetjs = require('./macrofolder/zxc.js')
+    console.log(agetjs)
+    console.log("aasd : " + agetjs.func123)
     let stopmacro = require(`/home/hosting_users/solverduo/apps/solverduo_solverduo/macrofolder/${name}.js`)
     stopmacro.boolean = true
     fs.unlink(`/home/hosting_users/solverduo/apps/solverduo_solverduo/macrofolder/${name}.js`, (err) => {
